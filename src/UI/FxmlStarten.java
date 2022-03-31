becbe7e68d825fcb715d8587702016a526779abd
Levent <levent.sona@gmail.com>
2022-03-31 12:17:04 +0200

    Resource Ordner eingebettet in Projekt

--- src/UI/FxmlStarten.java
+++ src/UI/FxmlStarten.java
@@ -8,12 +8,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class FxmlStarten extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FxmlStarten.class.getResource("HauptmenuV2.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        //FXMLLoader fxmlLoader = new FXMLLoader(FxmlStarten.class.getResource("HauptmenuV2.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/resource/HauptmenuV2.fxml")));
+        Scene scene = new Scene(root, 1080, 720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/UI/UIController.java
+++ src/UI/UIController.java
@@ -1,17 +1,35 @@
 package UI;
 
+import controller.RezeptkopfController;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class UIController {
 
     public Button btnStartProgrammBeenden;
+    public Button btnStartNeuRezept;
+    public Button btnStartDefinitionsbuch;
+
+    public ListView listStartRezepte;
+    public ListView listStartSuche;
+
+    public TextField textStartSuche;
+
+    RezeptkopfController rezeptkopfController = RezeptkopfController.getInstance();
+
+    public void rezeptListeUpdate(){
+        //listStartRezepte.add
+    }
 
     public void neuesRezeptFenster(ActionEvent actionEvent) {
+        //TODO neue FXML für "neues Rezept" einbinden und Starten
     }
 
     public void definitionsbuchOeffnen(ActionEvent actionEvent) {
+        //TODO neue FXML für "Definitionsbuch" einbinden und Starten
     }
 
     public void programmBeenden(ActionEvent actionEvent) {
